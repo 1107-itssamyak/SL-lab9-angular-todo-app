1507ccf1c82fded72c6e2753ef78e301c1dde124
1107-itssamyak <samyak.mehta11@gmail.com>
2021-11-13 19:09:27 +0530

    completed the app working by adding service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { TodoServiceService } from './todo-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-app';
+  constructor(private info: TodoServiceService) {
+  }
+
+  submit(e: any) {
+    if (e !== "") {
+      let id = this.info.todolist.length + 1;
+      let element = { "sno": id, "Task": e, "Done": "Pending" }
+      this.info.todolist.push(element);
+    }
+  }
+
+  removeTask(item: any) {
+    this.info.todolist.splice(this.info.todolist.indexOf(item), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todo-details/todo-details.component';
+import { TodoServiceService } from './todo-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [TodoServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
